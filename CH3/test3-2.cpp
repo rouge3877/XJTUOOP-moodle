fdd04c83576e94e0425a2124c329f911a3963c00
rouge <rouge_3877@outlook.com>
2024-04-05 16:04:46 +0800

    Make the output more detailed and formatted

--- CH3/test3-2.cpp
+++ CH3/test3-2.cpp
@@ -13,147 +13,205 @@
 #include <iostream>
 #include <cstring>
 
-class MyClass {
+class MyClass
+{
 public:
     static int staticData;
     int integerData;
     double doubleData;
-    char* charPtrData;
+    char *charPtrData;
     std::string stringData;
-    int& refData;
+    int &refData;
 
 public:
     // Constructor with dynamic memory allocation
-    MyClass(int intValue, double doubleValue, const char* charValue, const std::string& stringValue, int& ref)
-        : integerData(intValue), doubleData(doubleValue), stringData(stringValue), refData(ref) {
+    MyClass(int intValue, double doubleValue, const char *charValue, const std::string &stringValue, int &ref)
+        : integerData(intValue), doubleData(doubleValue), stringData(stringValue), refData(ref)
+    {
         charPtrData = new char[strlen(charValue) + 1];
         strcpy(charPtrData, charValue);
-        std::cout << "------ Construct ------" << std::endl;
+        std::cout << "    "
+                  << "    "
+                  << "++++++ Construct " << charPtrData << " is created ++++++" << std::endl;
+    }
+
+    MyClass(const MyClass &obj)
+        : integerData(obj.integerData), doubleData(obj.doubleData), stringData(obj.stringData), refData(obj.refData)
+    {
+        charPtrData = new char[strlen(obj.charPtrData) + 1];
+        strcpy(charPtrData, obj.charPtrData);
+        std::cout << "    "
+                  << "    "
+                  << "++++++ Copy Construct " << charPtrData << " is created ++++++" << std::endl;
     }
 
     // Destructor
-    ~MyClass() {
+    ~MyClass()
+    {
+        std::cout << "    "
+                  << "    "
+                  << "++++++ Destruct " << charPtrData << " is deleted ++++++" << std::endl;
         delete[] charPtrData;
-        std::cout << "------ Destruct ------" << std::endl;
     }
 
     // Static member function
-    static void staticFunction() {
+    static void staticFunction()
+    {
         std::cout << "Static function called" << std::endl;
     }
 
     // Non-static member function
-    void nonStaticFunction() {
+    void nonStaticFunction()
+    {
         std::cout << "Non-static function called" << std::endl;
     }
 
-    void printData() {
+    void printData()
+    {
 
-        std::cout << "    " << "------ Sec#1:  Data of object ------" << std::endl;
-        
-        std::cout << "    " << "in Object: " << this << std::endl;
-        std::cout << "    " << "Integer data: " << integerData << std::endl;
-        std::cout << "    " << "Double data: " << doubleData << std::endl;
-        std::cout << "    " << "Char pointer data: " << charPtrData << std::endl;
-        std::cout << "    " << "String data: " << stringData << std::endl;
+        std::cout << "  "
+                  << "------ Sec#1:  Data of object ------" << std::endl;
+
+        std::cout << "    "
+                  << "in Object: " << this << std::endl;
+        std::cout << "    "
+                  << "Integer data: " << integerData << std::endl;
+        std::cout << "    "
+                  << "Double data: " << doubleData << std::endl;
+        std::cout << "    "
+                  << "Char pointer data: " << charPtrData << std::endl;
+        std::cout << "    "
+                  << "String data: " << stringData << std::endl;
     }
 
-    void printDataAddress() {
-        std::cout << "    " << "------ Sec#2: Address of data ------" << std::endl;
-        
-        std::cout << "    " << "in Object: " << this << std::endl;
-        std::cout << "    " << "Integer data: " << &integerData << std::endl;
-        std::cout << "    " << "Double data: " << &doubleData << std::endl;
-        std::cout << "    " << "Char pointer data: " << reinterpret_cast<void*>(charPtrData) << std::endl;
-        std::cout << "    " << "String data: " << &stringData << std::endl;
-    }
+    void printDataAddress()
+    {
+        std::cout << "  "
+                  << "------ Sec#2: Address of data ------" << std::endl;
 
+        std::cout << "    "
+                  << "in Object: " << this << std::endl;
+        std::cout << "    "
+                  << "Integer data: " << &integerData << std::endl;
+        std::cout << "    "
+                  << "Double data: " << &doubleData << std::endl;
+        std::cout << "    "
+                  << "Char pointer data: " << reinterpret_cast<void *>(charPtrData) << std::endl;
+        std::cout << "    "
+                  << "String data: " << &stringData << std::endl;
+    }
 };
-
 
 // Initializing static data member
 int MyClass::staticData = -1010;
 
 // Global objects
-MyClass globalObj1(1, 2.3, "Global1", "Object", MyClass::staticData );
-MyClass globalObj2(2, 3.7, "Global2", "Object", MyClass::staticData );
+MyClass globalObj1(1, 2.3, "Global 1", "Object", MyClass::staticData);
+MyClass globalObj2(2, 3.7, "Global 2", "Object", MyClass::staticData);
 
 // External function
-void func(MyClass obj) {
-    std::cout << "Call external func() with object" << std::endl;
-    std::cout << "Inside External func():" << std::endl;
-    std::cout << "Object address: " << &obj << std::endl;
+void func(MyClass obj)
+{
+    obj.printData();
+    obj.printDataAddress();
 }
 
-int main() {
+int main()
+{
+    std::cout << "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ MAIN FUNCTION BEGIN: $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+              << std::endl;
+
+    std::cout << "\n---------------------- Para#0:  Initialization ---------------------" << std::endl;
     // Local objects in main()
-    MyClass localObj1(3, 4.9, "Local1", "Object", MyClass::staticData );
-    MyClass localObj2(4, 5.1, "Local2", "Object", MyClass::staticData );
+    MyClass localObj1(3, 4.9, "Local  1", "Object", MyClass::staticData);
+    MyClass localObj2(4, 5.1, "Local  2", "Object", MyClass::staticData);
+    // Dynamic objects in main()
+    MyClass *dynamicObj1 = new MyClass(5, 6.3, "Dynamic1", "Object", MyClass::staticData);
+    MyClass *dynamicObj2 = new MyClass(6, 7.2, "Dynamic2", "Object", MyClass::staticData);
+    std::cout << "--------------------------------END---------------------------------\n"
+              << std::endl;
 
     // Calling external function func()
+    std::cout << "\n------------------ Para#1:  Inside External func() -----------------" << std::endl;
+    std::cout << "    "
+              << "Call external func(): " << &func << "  with object: " << &localObj1 << std::endl;
     func(localObj1);
-    func(localObj2);
+    std::cout << "--------------------------------END---------------------------------\n"
+              << std::endl;
 
-    // Dynamic objects in main()
-    MyClass* dynamicObj1 = new MyClass(5, 6.3, "Dynamic1", "Object", MyClass::staticData );
-    MyClass* dynamicObj2 = new MyClass(6, 7.2, "Dynamic2", "Object", MyClass::staticData );
+    std::cout << "\n------------------ Para#2:     Inside     main() -------------------" << std::endl;
+    std::cout << "    "
+              << "Global  object 1 address: " << &globalObj1 << std::endl;
+    std::cout << "    "
+              << "Global  object 2 address: " << &globalObj2 << std::endl;
+    std::cout << "    "
+              << "Local   object 1 address: " << &localObj1 << std::endl;
+    std::cout << "    "
+              << "Local   object 2 address: " << &localObj2 << std::endl;
+    std::cout << "    "
+              << "Dynamic object 1 address: " << dynamicObj1 << std::endl;
+    std::cout << "    "
+              << "Dynamic object 2 address: " << dynamicObj2 << std::endl;
 
-    std::cout << "\n------------------ Para#1:     Inside     main() ------------------" << std::endl;
-    std::cout << "    "  << "Global object 1 address: " << &globalObj1 << std::endl;
-    std::cout << "    "  << "Global object 2 address: " << &globalObj2 << std::endl;
-    std::cout << "    "  << "Local object 1 address: " << &localObj1 << std::endl;
-    std::cout << "    "  << "Local object 2 address: " << &localObj2 << std::endl;
-    std::cout << "    "  << "Dynamic object 1 address: " << dynamicObj1 << std::endl;
-    std::cout << "    "  << "Dynamic object 2 address: " << dynamicObj2 << std::endl;
+    std::cout << "    "
+              << "Size of MyClass: " << sizeof(MyClass) << " bytes" << std::endl;
+    std::cout << "-------------------------------END----------------------------------\n"
+              << std::endl;
 
-    std::cout << "    "  << "Size of MyClass: " << sizeof(MyClass) << " bytes" << std::endl;
-    std::cout << "-------------------------------END----------------------------------\n" << std::endl;
-
-    std::cout << "\n------------------ Para#2: Details of  globalObj1 ------------------" << std::endl;
+    std::cout << "\n------------------ Para#3: Details of  globalObj1 ------------------" << std::endl;
     globalObj1.printData();
     globalObj1.printDataAddress();
-    //print function address
-    void(MyClass::*pmf)() = &MyClass::nonStaticFunction;
-    unsigned * p0 = (unsigned *)&pmf;
-    std::cout << "    " << "nonStaticFunction address: " << std::hex << std::noshowbase << *p0 << std::endl;
-    //print static function address
-    void(*pf)() = &MyClass::staticFunction;
-    unsigned * p1 = (unsigned *)&pf;
-    std::cout << "    " << "staticFunction address: " << std::hex << std::noshowbase << *p1 << std::endl;
-    std::cout << "-------------------------------END----------------------------------\n" << std::endl;
+    // print function address
+    void (MyClass::*pmf)() = &MyClass::nonStaticFunction;
+    unsigned *p0 = (unsigned *)&pmf;
+    std::cout << "    "
+              << "nonStaticFunction address: " << std::hex << std::noshowbase << *p0 << std::endl;
+    // print static function address
+    void (*pf)() = &MyClass::staticFunction;
+    unsigned *p1 = (unsigned *)&pf;
+    std::cout << "    "
+              << "staticFunction address: " << std::hex << std::noshowbase << *p1 << std::endl;
+    std::cout << "-------------------------------END----------------------------------\n"
+              << std::endl;
 
-    
-    
-    std::cout << "\n------------------ Para#3:  Details of  localObj1 ------------------" << std::endl;
+    std::cout << "\n------------------ Para#4:  Details of  localObj1 ------------------" << std::endl;
     localObj1.printData();
     localObj1.printDataAddress();
-    //print function address
-    void(MyClass::*pmf1)() = &MyClass::nonStaticFunction;
-    unsigned * p2 = (unsigned *)&pmf1;
-    std::cout << "    " << "nonStaticFunction address: " << std::hex << std::noshowbase << *p2 << std::endl;
-    //print static function address
-    void(*pf1)() = &MyClass::staticFunction;
-    unsigned * p3 = (unsigned *)&pf1;
-    std::cout << "    " << "staticFunction address: " << std::hex << std::noshowbase << *p3 << std::endl;
-    std::cout << "-------------------------------END----------------------------------\n" << std::endl;
+    // print function address
+    void (MyClass::*pmf1)() = &MyClass::nonStaticFunction;
+    unsigned *p2 = (unsigned *)&pmf1;
+    std::cout << "    "
+              << "nonStaticFunction address: " << std::hex << std::noshowbase << *p2 << std::endl;
+    // print static function address
+    void (*pf1)() = &MyClass::staticFunction;
+    unsigned *p3 = (unsigned *)&pf1;
+    std::cout << "    "
+              << "staticFunction address: " << std::hex << std::noshowbase << *p3 << std::endl;
+    std::cout << "-------------------------------END----------------------------------\n"
+              << std::endl;
 
-
-    std::cout << "\n------------------ Para#4: Details of dynamicObj1 ------------------" << std::endl;
+    std::cout << "\n------------------ Para#5: Details of dynamicObj1 ------------------" << std::endl;
     dynamicObj1->printData();
     dynamicObj1->printDataAddress();
-    //print function address
-    void(MyClass::*pmf2)() = &MyClass::nonStaticFunction;
-    unsigned * p4 = (unsigned *)&pmf2;
-    std::cout << "    " << "nonStaticFunction address: " << std::hex << std::noshowbase << *p4 << std::endl;
-    //print static function address
-    void(*pf2)() = &MyClass::staticFunction;
-    unsigned * p5 = (unsigned *)&pf2;
-    std::cout << "    " << "staticFunction address: " << std::hex << std::noshowbase << *p5 << std::endl;
-    std::cout << "-------------------------------END----------------------------------\n" << std::endl;
+    // print function address
+    void (MyClass::*pmf2)() = &MyClass::nonStaticFunction;
+    unsigned *p4 = (unsigned *)&pmf2;
+    std::cout << "    "
+              << "nonStaticFunction address: " << std::hex << std::noshowbase << *p4 << std::endl;
+    // print static function address
+    void (*pf2)() = &MyClass::staticFunction;
+    unsigned *p5 = (unsigned *)&pf2;
+    std::cout << "    "
+              << "staticFunction address: " << std::hex << std::noshowbase << *p5 << std::endl;
+    std::cout << "-------------------------------END----------------------------------\n"
+              << std::endl;
 
     // Freeing dynamic objects
-    // delete dynamicObj1;
-    // delete dynamicObj2;
+    delete dynamicObj1;
+    delete dynamicObj2;
+
+    std::cout << "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ MAIN FUNCTION END $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+              << std::endl;
 
     return 0;
 }
